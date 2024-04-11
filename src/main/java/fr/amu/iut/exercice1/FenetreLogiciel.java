38d1825a1db21783f6125a813f916a065a835e87
Manon VERHILLE <manon.verhille@etu.univ-amu.fr>
2024-04-11 12:18:07 +0200

    First commit

--- src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
+++ src/main/java/fr/amu/iut/exercice1/FenetreLogiciel.java
@@ -1,9 +1,11 @@
 package fr.amu.iut.exercice1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -38,12 +40,54 @@ public class FenetreLogiciel extends Application {
         MenuBar menuBar = new MenuBar(menu1, menu2, menu3);
 
         topMenu.getChildren().addAll(menuBar);
-        // Création de la zone Boutons
-        // HBox boutons = new
 
+        // Création milieu
+        HBox milieu = new HBox();
+
+        // Création de la zone Boutons
+        VBox boutons = new VBox();
+        boutons.setAlignment(Pos.CENTER);
+        Label l1 = new Label("Boutons");
+        Button bouton1 = new Button("Bouton 1");
+        Button bouton2 = new Button("Bouton 2");
+        Button bouton3 = new Button("Bouton 3");
+
+        boutons.getChildren().addAll(l1, bouton1, bouton2, bouton3);
+        boutons.setPadding(new Insets(Pos.CENTER.ordinal(), Pos.CENTER.ordinal(), Pos.CENTER.ordinal(), Pos.CENTER.ordinal()));
 
         // Création de la ligne de séparation
-        Separator sep = new Separator();
+        Separator sep1 = new Separator();
+
+        // Création du menu de connexion
+        VBox connexion = new VBox();
+        connexion.setAlignment(Pos.CENTER);
+        GridPane formulaire = new GridPane();
+        formulaire.setAlignment(Pos.CENTER);
+        formulaire.setHgap(10);
+        formulaire.setVgap(10);
+        Label name = new Label("Name");
+        TextField champ1 = new TextField();
+        Label email = new Label("Email");
+        TextField champ2 = new TextField();
+        Label password = new Label("Password");
+        TextField champ3 = new TextField();
+
+        formulaire.add(name, 0, 0);
+        formulaire.add(champ1, 1, 0);
+        formulaire.add(email, 0, 1);
+        formulaire.add(champ2, 1, 1);
+        formulaire.add(password, 0, 2);
+        formulaire.add(champ3, 1, 2);
+
+        Button submit = new Button("Submit");
+        Button cancel = new Button("Cancel");
+
+        connexion.getChildren().addAll(formulaire, submit, cancel);
+
+        // Création de la ligne de séparation
+        Separator sep2 = new Separator();
+
+        milieu.getChildren().addAll(boutons, connexion);
 
         // Création du bandeau en bas de la fenêtre
         HBox basPage = new HBox();
@@ -55,7 +99,9 @@ public class FenetreLogiciel extends Application {
         // Ajout des contrôleurs au conteneur principal
         vbox.getChildren().addAll(
                 topMenu,
-                sep,
+                milieu,
+                sep1,
+                sep2,
                 basPage
         );
 
